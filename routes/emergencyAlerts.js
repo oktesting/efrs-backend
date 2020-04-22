5ef625fe108f8053d66cd7ed721a0c30e9a6e387
oktesting <duczoz123@gmail.com>
2020-04-22 23:28:05 +0700

    implement send emergency alert feature

--- routes/emergencyAlerts.js
+++ routes/emergencyAlerts.js
@@ -6,6 +6,8 @@ const {
   EmergencyAlert,
   validateEmergencyAlert,
 } = require("../models/emergencyAlert");
+const { Location } = require("../models/location");
+const { sendAlert } = require("../services/sendAlert");
 const express = require("express");
 const router = express.Router();
 
@@ -22,9 +24,56 @@ router.post(
       title: req.body.title,
       supervisor: req.body.supervisor,
     });
-    return res.status(200).send(await alert.save());
-    // viết logic notify tất cả user nằm trong vùng bán kính ở đây
+    await alert.save();
+    //get all locations in db except fire station and location without device
+    const locations = await Location.find({
+      isFireStation: false,
+      device: { $ne: null },
+    });
+
+    //get all location within the range
+    const filteredLocations = locations.filter(
+      (location) =>
+        getDistanceFrom2CoordinatesInKm(
+          alert.lat,
+          alert.lng,
+          location.lat,
+          location.lng
+        ) <= alert.radius
+    );
+
+    //get array of token's device
+    let registrationTokens = [];
+    filteredLocations.forEach((location) => {
+      registrationTokens.push(location.device.registrationToken);
+    });
+
+    //remove duplicated token
+    registrationTokens = [...new Set(registrationTokens)];
+
+    await sendAlert(alert, registrationTokens);
+
+    return res.status(200).send(filteredLocations);
   }
 );
+
+function getDistanceFrom2CoordinatesInKm(lat1, lng1, lat2, lng2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lng2 - lng1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
 
 module.exports = router;
---
+++ services/sendAlert.js
@@ -0,0 +1,74 @@
+const gcm = require("node-gcm");
+const config = require("config");
+
+module.exports.sendAlert = async (alert, registrationTokens) => {
+  // ... or some given values
+  const message = new gcm.Message({
+    //   collapseKey: "demo",
+    priority: "high",
+    contentAvailable: true,
+    delayWhileIdle: true,
+    timeToLive: 10,
+    restrictedPackageName: "somePackageName",
+    dryRun: true,
+    data: {
+      title: alert.title,
+      message: alert.message,
+    },
+    notification: {
+      title: alert.title,
+      icon: "ic_launcher",
+      body: alert.message,
+    },
+  });
+
+  // Set up the sender with you API key
+  const sender = new gcm.Sender(config.get("firebase_server_api_key"));
+
+  // Add the registration tokens of the devices you want to send to
+  //   var registrationTokens = [];
+  //   registrationTokens.push("regToken1");
+  //   registrationTokens.push("regToken2");
+
+  // Send the message
+  // ... trying only once
+  //   sender.sendNoRetry(
+  //     message,
+  //     { registrationTokens: registrationTokens },
+  //     function (err, response) {
+  //       if (err) console.error(err);
+  //       else console.log(response);
+  //     }
+  //   );
+
+  // ... or retrying
+  //   sender.send(message, { registrationTokens: registrationTokens }, function (
+  //     err,
+  //     response
+  //   ) {
+  //     if (err) console.error(err);
+  //     else console.log(response);
+  //   });
+
+  // ... or retrying a specific number of times (10)
+  sender.send(message, { registrationTokens: registrationTokens }, 5, function (
+    err,
+    response
+  ) {
+    if (err) console.error(err);
+    else console.log(response);
+  });
+
+  // Q: I need to remove all "bad" token from my database, how do I do that?
+  //    The results-array does not contain any tokens!
+  // A: The array of tokens used for sending will match the array of results, so you can cross-check them.
+  //   sender.send(message, { registrationTokens: registrationTokens }, function (
+  //     err,
+  //     response
+  //   ) {
+  //     var failed_tokens = registrationTokens.filter(
+  //       (token, i) => response[i].error != null
+  //     );
+  //     console.log("These tokens are no longer ok:", failed_tokens);
+  //   });
+};
